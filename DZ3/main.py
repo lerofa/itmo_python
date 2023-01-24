f5361845e6bf330bfee60e47ced076a2c461e844
Roman <roman@gittest.ru>
2023-01-24 19:20:48 +0300

    ДЗ №3 с исправлениями

--- DZ3/main.py
+++ DZ3/main.py
@@ -1,4 +1,4 @@
-
+from typing import Set, Any
 
 string = "Python is the best programming language in the world"
 print(string[6:-7])
@@ -7,7 +7,10 @@ string = "Guido van Rossum is the benevolent dictator for life"
 print(string[2::3])
 
 string = "You have a problem with authority, Mr. Anderson."
-uniq_chars = set(string)
 
+# Без учета регистра
+# string = string.lower()
+
+uniq_chars = set(string)
 d = dict(zip(uniq_chars, map(string.count, uniq_chars)))
 print(d)
